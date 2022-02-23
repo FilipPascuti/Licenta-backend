98069d32162ba40d79e5ba63213a4ebf617b8e76
Filip Pascuti <filip@Filips-Mac-mini.local>
2022-02-23 10:23:51 +0200

    added remote db

--- api/deps.py
+++ api/deps.py
@@ -12,7 +12,7 @@ from core.config import settings
 from db.session import SessionLocal
 
 reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_STR}/login"
+    tokenUrl=f"{settings.API_STR}/auth/login"
 )
 
 
--- core/config.py
+++ core/config.py
@@ -12,7 +12,9 @@ class Settings(BaseSettings):
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    DATABASE_URI: str = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/licenta"
+    # DATABASE_URI: str = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/licenta"
+    DATABASE_URI: str = "postgresql+psycopg2://ofqkorzsuftujt:0e482520c1959fc7dffbf5e0390f10f512e67ba1c9699c713ea83e45e7b26250@ec2-54-170-212-187.eu-west-1.compute.amazonaws.com:5432/dapoqt4ebioh8o"
+
     SHOW_DDL = False
 
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:3000"]
